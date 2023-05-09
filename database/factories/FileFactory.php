bc71e8e10b0e435fae2a31a584693832af72d9ed
Lenovo <marianknazo@gmail.com>
2023-05-09 14:23:10 +0200

    nothing new 2

--- database/factories/FileFactory.php
+++ database/factories/FileFactory.php
@@ -24,6 +24,7 @@ class FileFactory extends Factory
             'path' => $this->faker->filePath(),
             'date' => now(),
             'points' =>  $this->faker->randomDigit(),
+
         ];
     }
 
