d6a22a1a25ae98f2930c635e65f01783288b02ce
Lenovo <marianknazo@gmail.com>
2023-05-10 11:06:52 +0200

    assigning of a task to student

--- app/Http/Controllers/AssignTasksController.php
+++ app/Http/Controllers/AssignTasksController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
 class AssignTasksController extends Controller
@@ -19,6 +20,19 @@ class AssignTasksController extends Controller
         $data = compact('id','query');
 
         return view('assigntasks')->with($data);
+
+    }
+
+    public function insert( Request $request){
+
+
+        //$query = DB::insert('insert into student_tasks st values ()');
+
+        //$data = compact('id','query');
+
+        var_dump(dd($request->all()));
+
+        return view('index');
 
     }
 
--- resources/views/assigntasks.blade.php
+++ resources/views/assigntasks.blade.php
@@ -6,33 +6,33 @@
 
     <div class=" table-responsive col-xl-6">
         <div class="text-center">
-            <h3><b>Štatistiky študentov</b></h3>
+            <h3><b>Pridarenie úlohy študentovi</b></h3>
         </div>
 
-        <table id="stats" class="table table-striped table-bordered table-hover border-dark text-center">
-            <thead class="bg-dark text-white">
-            <tr class="text-center">
-                <th>ID</th>
-                <th>Cesta</th>
-                <th>Body</th>
-                <th>Obmedzenie</th>
-                <th>Výber</th>
-            </tr>
-            </thead>
-            <tbody>
-            @foreach($query as $item)
-                <tr>
-                    <td>{{$item->id}}</td>
-                    <td>{{$item->path}}</td>
-                    <td><input type="number" value="{{$item->points}}"></td>
-                    <td><input type="date" value="{{$item->date}}"></td>
-                    <td><input type="checkbox"></td>
-                </tr>
-            @endforeach
-            </tbody>
-        </table>
-    </div>
 
-    <button type="button" class="btn btn-success">pridaj</button>
+            <table id="stats" class="table table-striped table-bordered table-hover border-dark text-center">
+                <thead class="bg-dark text-white">
+                <tr class="text-center">
+                    <th>ID</th>
+                    <th>Cesta</th>
+                    <th>Body</th>
+                    <th>Obmedzenie</th>
+                    <th>Pridaj</th>
+                </tr>
+                </thead>
+                <tbody>
+                @foreach($query as $item)
+                    <tr>
+                        <td>{{$item->id}}</td>
+                        <td>{{$item->path}}</td>
+                        <td>{{$item->points}}</td>
+                        <td>{{$item->date}}</td>
+                        <td> <form action="{{url('assigntasks')}}" method="post"><input type="hidden" name="task_id" value="{{$item->id}}"><button type="submit" class="btn btn-success">Pridaj</button></form></td>
+                    </tr>
+                @endforeach
+                </tbody>
+            </table>
+
+    </div>
 
 @endsection
