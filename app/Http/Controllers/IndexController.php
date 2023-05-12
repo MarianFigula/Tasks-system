d2ad2e0a5adece2a002c34600000d7c9d6043d84
Marian Figula <majofigulam3@gmail.com>
2023-05-12 18:04:12 +0200

    Changed directory of images + parsing data from latex

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -10,11 +10,38 @@ class IndexController extends Controller
         $this->middleware('auth');
     }
 
+    public static function getStringInCurlyBraces($string): string
+    {
+        $start = strpos($string, '{') + 1;
+        $length = strpos($string, '}') - $start;
+        $substring = substr($string, $start, $length); // "B34A5A"
+        return $substring;
+    }
+
+    /*
+    public static function showFrac($string): string
+    {
+        $string = preg_replace('/(\$.*?\$)/', '[$1]', $string);
+        $string = preg_replace('/([a-zA-Z])([\(\)\[\]\-+*\/=])/', '$1 $2', $string);
+        $string = preg_replace('/([\(\)\[\]\-+*\/=])([a-zA-Z])/', '$1 $2', $string);
+
+        return $string;
+    }*/
+
+    public static function displayEquation($string){
+        //$pos = strpos( $string, '$');
+        $string = preg_replace('/\$/', '\[', $string, 1);
+        $string = preg_replace('/\$/', '\]', $string, 1);
+        return $string;
+    }
+
+
+
     public function index()
     {
         $filePath = public_path('../priklady/blokovka01pr.tex');
         $latexContent = File::get($filePath);
-        $latexContent = str_replace('$', '', $latexContent);
+        //$latexContent = str_replace('$', '', $latexContent);
         $latexLines = explode("\n", $latexContent);
         return view('index', ['latexLines' => $latexLines]);
     }
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -15,7 +15,6 @@
     <!-- To automatically render math in text elements, include the auto-render extension: -->
     <script defer src="https://cdn.jsdelivr.net/npm/katex@0.16.7/dist/contrib/auto-render.min.js" integrity="sha384-+VBxd3r6XgURycqtZ117nYw44OOcIax56Z4dCRWbxyPt0Koah1uHoK0o4+/RRE05" crossorigin="anonymous"
             onload="renderMathInElement(document.body);"></script>
-
 </head>
 <body>
 TUTO ZMENU SPRAVIL MARIAN K.
@@ -27,27 +26,49 @@ TUTO ZMENU SPRAVIL ANTRIK.
 
 </p>
 
-{{--$latex_code = file_get_contents('C:\Users\ajoaj\PhpstormProjects\webte2\zaverecneZadanieWebte\priklady\blokovka01pr.tex')--}}
-
+{{--$latex_code = file_get_contents('C:\Users\ajoaj\PhpstormProjects\webte2\zaverecneZadanieWebte\zadanie99\blokovka01pr.tex')--}}
+<img src="{{asset('zadanie99/images/blokovka02_00004.jpg')}}" alt="image">
 
 <div class="katex">
+    {{-- TODO ULOZIT CISLO DO DB, COUNTER PRE POCET ULOH, RNG, --}}
+
     @foreach ($latexLines as $line)
-        {!! '\[' . $line . '\]' !!}<br>
+        {{-- // zobrazenie ideciek - na ulozenie do db podla random number gen//
+        @if(\Illuminate\Support\Str::contains($line, "\section*{"))
+            {{ $task_num = \App\Http\Controllers\IndexController::getTaskId($line) }}
+            <br>
+        @endif --}}
+        @if(\Illuminate\Support\Str::contains($line, "$"))
+            {!! $text =  \App\Http\Controllers\IndexController::displayEquation($line) !!}
+
+            @continue
+        @elseif(\Illuminate\Support\Str::contains($line, "\includegraphics"))
+            {{-- TODO CESTA IDE V STRINGU ALE NIE VEZ FUNCKIU - VARDUMP UKAZUJE MALO ZNAKOV ALE VYPISE SPRAVNE--}}
+            <p>{{ \App\Http\Controllers\IndexController::getStringInCurlyBraces($line) }}</p>
+            <img src="{{asset("'". \App\Http\Controllers\IndexController::getStringInCurlyBraces($line))."'"}}" alt="image">
+        @endif
+
+
+        {{-- TODO: TU JE ZAZNAMENANA ODPOVED - ulozit do databazy --}}
+        @if(\Illuminate\Support\Str::contains($line, "\dfrac{"))
+            {!! '\[' . $line . '\]' !!}
+        @endif
+        {{--{!! '\[' . $line . '\]' !!} --}}
     @endforeach
 </div>
 
 <script src="https://cdnjs.cloudflare.com/ajax/libs/KaTeX/0.13.11/katex.min.js" integrity="sha384-9lsoyz3sJX5k8Ywd+yPitnjqFDfsgawBTWV5O/Vk+T5P/16g0op7bUEj4ikO8yl0" crossorigin="anonymous"></script>
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/KaTeX/0.13.11/katex.min.css" integrity="sha384-9R0MLYyGQOEF0M0sZFK8M7EedNh1A4A/JseZTQNYXWdWUtmIh7Qc9mJX5WTG+9BZ" crossorigin="anonymous" />
 <script>
-    document.addEventListener("DOMContentLoaded", function() {
-        renderMathInElement(document.querySelector(".katex"), {
-            delimiters: [
-                {left: "$$", right: "$$", display: true},
-                {left: "$", right: "$", display: false},
-            ],
-            processEscapes: true
-        });
-    });
+document.addEventListener("DOMContentLoaded", function() {
+renderMathInElement(document.querySelector(".katex"), {
+    delimiters: [
+        //{left: "$$", right: "$$", display: true},
+        //{left: "$", right: "$", display: false},
+    ],
+    processEscapes: true
+});
+});
 </script>
 
 
