a971535b1f09aa62ba9fdbda315bc0c855cc46c3
Marian Figula <majofigulam3@gmail.com>
2023-05-13 09:59:03 +0200

    added button with extending layouts.app

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,33 +1,7 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/katex@0.16.7/dist/katex.min.css" integrity="sha384-3UiQGuEI4TTMaFmGIZumfRPtfKQ3trwQE2JgosJxCnGmQpL/lJdjpcHkaaFwHlcI" crossorigin="anonymous">
-
-    <!-- The loading of KaTeX is deferred to speed up page rendering -->
-    <script defer src="https://cdn.jsdelivr.net/npm/katex@0.16.7/dist/katex.min.js" integrity="sha384-G0zcxDFp5LWZtDuRMnBkk3EphCK1lhEf4UEyEM693ka574TZGwo4IWwS6QLzM/2t" crossorigin="anonymous"></script>
-
-    <!-- To automatically render math in text elements, include the auto-render extension: -->
-    <script defer src="https://cdn.jsdelivr.net/npm/katex@0.16.7/dist/contrib/auto-render.min.js" integrity="sha384-+VBxd3r6XgURycqtZ117nYw44OOcIax56Z4dCRWbxyPt0Koah1uHoK0o4+/RRE05" crossorigin="anonymous"
-            onload="renderMathInElement(document.body);"></script>
-</head>
-<body>
-TUTO ZMENU SPRAVIL MARIAN K.
-TUTO ZMENU SPRAVIL ANTRIK.
-
-<p>
-
-    \[F(s)=\dfrac{Y(s)}{W(s)}.\]
-
-</p>
+@extends('layouts.app')
 
 {{--$latex_code = file_get_contents('C:\Users\ajoaj\PhpstormProjects\webte2\zaverecneZadanieWebte\zadanie99\blokovka01pr.tex')--}}
-
+@section('content')
 <div class="katex">
     {{-- TODO ULOZIT CISLO DO DB, COUNTER PRE POCET ULOH, RNG, --}}
 
@@ -56,10 +30,15 @@ TUTO ZMENU SPRAVIL ANTRIK.
     @endforeach
 </div>
 
-<div>
-    <input type="text" class="p-3" placeholder="Answer">
-</div>
+<form action="" method="post">
+    <div class="container input-group mb-3">
+        <label for="answer-id">Odpoveď<input type="text" id="answer-id" class="form-control"></label>
+    </div>
+    <div class="container input-group mb-3">
+        <button class="btn btn-success" type="submit">Odpovedať</button>
+    </div>
 
+</form>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/KaTeX/0.13.11/katex.min.js" integrity="sha384-9lsoyz3sJX5k8Ywd+yPitnjqFDfsgawBTWV5O/Vk+T5P/16g0op7bUEj4ikO8yl0" crossorigin="anonymous"></script>
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/KaTeX/0.13.11/katex.min.css" integrity="sha384-9R0MLYyGQOEF0M0sZFK8M7EedNh1A4A/JseZTQNYXWdWUtmIh7Qc9mJX5WTG+9BZ" crossorigin="anonymous" />
 <script>
@@ -73,9 +52,4 @@ renderMathInElement(document.querySelector(".katex"), {
 });
 });
 </script>
-
-
-
-
-</body>
-</html>
+@endsection
