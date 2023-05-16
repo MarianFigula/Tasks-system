0a741e92376ce116c3cde084d29e6a46f445745e
Lenovo <marianknazo@gmail.com>
2023-05-16 12:24:33 +0200

    dopisany navod pre studenta

--- resources/views/studentTutorial.blade.php
+++ resources/views/studentTutorial.blade.php
@@ -10,11 +10,9 @@
 
         <p style="font-family: DejaVu Sans;">Po prihlásení sa študentovi zobrazí prehľadná tabuľka priradených úloh.</p>
 
-        <p style="font-family: DejaVu Sans;">Po kliknutí na tlačidlo detail sa zobrazia podrobné výsledky daného študenta.</p>
+        <p style="font-family: DejaVu Sans;">Po kliknutí na tlačidlo Riešiť sa študentovi vygeneruje príklad na riešenie.</p>
 
-        <p style="font-family: DejaVu Sans;">Po kliknutí na tlačidlo priradiť sa zobrazí prehľad na priradenie ďalších úloh študentovi.</p>
-
-        <p style="font-family: DejaVu Sans;">V navigačnej lište sa nachádzajú odkazy na Návod na použitie, Domovskú stránku a Súbory s úlohami.</p>
+        <p style="font-family: DejaVu Sans;">V navigačnej lište sa nachádzajú odkazy na Návod na použitie a Domovskú stránku.</p>
     @show
 
     <p style="font-family: DejaVu Sans;">dlasi odstave, musi tu byt takyto font kvoli pdf utf-8</p>
